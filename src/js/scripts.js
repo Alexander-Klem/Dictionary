2eb63809c59ea1971d557f13a608fccb698ae98a
Alexander <a.klemyato@rsm.by>
2025-01-23 12:24:18 +0300

    Проект без сохранения(2)

---
+++ src/js/scripts.js
@@ -0,0 +1,121 @@
+// TODO: получить данные по API
+// TODO: вставить слово в контейнер results-word
+// TODO: добавить функционал для воспроизведения звука
+// TODO: вставить полученные данные в конейнер с результатами
+
+let state = {
+    word: ``,
+    meanings: [],
+    phonetics: []
+}
+
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const input = document.getElementById(`word-input`);
+const form = document.querySelector(`.form`);
+const containerWord = document.querySelector(`.results-word`);
+const soundButton = document.querySelector('.results-sound');
+const resultsWrapper = document.querySelector(`.results`);
+const resultsList = document.querySelector(`.results-list`);
+const errorContainer = document.querySelector(`.error`);
+
+
+const showError = (error) => { 
+    errorContainer.style.display = `block`;
+    resultsWrapper.style.display = `none`;
+
+    errorContainer.innerText = error.message;
+}
+
+const renderDefinition = (itemDefinition) => { 
+    const example = itemDefinition.example ?
+        `<div class="results-item__example">
+        <p>Example: <span>${itemDefinition.example}</span></p>
+        </div>`
+        : ``;
+
+return `<div class="results-item__definition">
+        <p>${itemDefinition.definition}</p>
+        ${example}
+        </div>`
+}
+
+const getDefinitions = (definition) => {
+    return definition.map(renderDefinition).join('');
+}
+
+const renderItem = (item) => { 
+    // const itemDefinition = item.definitions[0];
+return ` <div class="results-item">
+            <div class="results-item__part">${item.partOfSpeech}</div>
+            <div class="results-item__definitions">
+            ${getDefinitions(item.definitions)}
+        </div>
+    </div>`
+}
+
+const showResults = () => { 
+    resultsWrapper.style.display = `block`;
+    resultsList.innerHTML = ``;
+    
+   state.meanings.forEach(item => {
+        resultsList.innerHTML += renderItem(item);
+    });
+}
+
+const insertWord = () => { 
+    containerWord.innerText = state.word;
+}
+
+
+const handleSubmit = async (event) => { 
+    event.preventDefault();
+
+    // if (!state.word.trim()) return;
+
+    try {
+        const response = await fetch(`${url} ${state.word}`);
+        const data = await response.json();
+
+        if (response.ok && data.length) {
+            errorContainer.style.display = `none`;
+            const item = data[0];
+
+            state = {
+                ...state,
+                meanings: item.meanings,
+                phonetics: item.phonetics,
+            }
+
+            insertWord();
+            showResults();
+        } else { 
+            showError(data);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+    
+}
+
+const handleKeyUp = (event) => {
+    const value = event.target.value;
+    state.word = value;
+};
+
+
+const handleSound = () => { 
+    if (state.phonetics.length) { 
+        const sound = state.phonetics[0];
+
+        if (sound.audio) { 
+            new Audio(sound.audio).play();
+        }
+    }
+}
+
+//EVENTS
+
+input.addEventListener(`keyup`, handleKeyUp);
+form.addEventListener(`submit`, handleSubmit);
+soundButton.addEventListener(`click`, handleSound);
